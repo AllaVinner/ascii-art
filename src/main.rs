e1c0fb0c5414567d455987a46e2740ccbb2903cb
AllaVinner <joel.w.ottosson@gmail.com>
2023-07-10 21:50:27 +0200

    Strucutred normalization and lighting into enums

--- src/main.rs
+++ src/main.rs
@@ -17,21 +17,48 @@ fn find_closest_match(pixel: f64, font_info: &FontInfo) -> char {
     best_char
 }
 
+enum Lighting {
+    DARK,
+    LIGHT
+}
+
+enum Normalization {
+    NONE,
+    POW(f64),
+    SYMMETRICPOW(f64),
+    DOUBLEPOW(f64, f64),
+
+}
+
 
 fn main() {
     println!("Hello, world!");
+    let input_image_path = "images/at_wedding.jpg";
+    let ascii_num_rows = 100;
+    let output_file_path = "out.txt";
+    let lighting = Lighting::DARK;
+    let normalization = Normalization::NONE;
+
     let font_info = font_mean_value::get_font_info();
-    let value_image = image_handler::get_value_image_from_rows("images/at_wedding.jpg", 1, font_info.hight_width_ratio);
+    let value_image = image_handler::get_value_image_from_rows(input_image_path, ascii_num_rows, font_info.hight_width_ratio);
     let mut s: String = "".to_string();
     for (i, p) in value_image.pixels().enumerate() {
         if i as u32 % value_image.width() == 0{
             s.push('\n');
         }
-        let mut val = (1.-p.0[0] as f64 / 255.);
-        let pow: f64 = 1.;
-        val = (1.- val.powf(pow)).powf(1./pow);
+        let mut val = p.0[0] as f64 / 255.;
+        val = match lighting {
+            Lighting::DARK => val,
+            Lighting::LIGHT => 1. - val,
+        };
+        val = match normalization {
+            Normalization::NONE => val,
+            Normalization::POW(p) => val.powf(p),
+            Normalization::SYMMETRICPOW(p) => (val.powf(p) + val.powf(1./p))/2.,
+            Normalization::DOUBLEPOW(tail, head) => (val.powf(tail) + val.powf(head))/2., 
+        };
         s.push(find_closest_match(val, &font_info))
         
     }
-    fs::write("out.txt", s).expect("Unable to write file");
+    fs::write(output_file_path, s).expect("Unable to write file");
 }
